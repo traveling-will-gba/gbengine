379c4a4eff8a1d17ad89f1a0018d2f6f2ef53fe1
Vitor Barbosa <vitornga15@gmail.com>
2018-05-15 23:26:13 -0300

    Add gbengine test examples

---
+++ test/input/src/main.c
@@ -0,0 +1,26 @@
+#include "video.h"
+#include "input.h"
+
+#define RED 0x0000FF
+
+unsigned short *vid_mem = (unsigned short *)0x6000000;
+
+int main() {
+	reset_dispcnt();
+	set_video_mode(3);
+	set_background_number(2);
+
+    while(1) {   
+        check_buttons_states();
+
+        for(int i=0;i<=9;i++){
+            if (pressed(i)) {
+                vid_mem[50 * 240 + i * 10] = RED;
+            } else {
+                vid_mem[50 * 240 + i * 10] = 0;
+            }
+        }
+    }   
+
+    return 0;
+}
