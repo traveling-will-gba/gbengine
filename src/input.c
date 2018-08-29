48e3bae51d00a8187bc9d637336dddfffbbd4bf6
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2018-08-28 23:05:50 -0300

    Start memory management

--- src/input.c
+++ src/input.c
@@ -3,7 +3,7 @@
 volatile unsigned int *buttons_mem = (volatile unsigned int *)0x04000130;
 
 void check_buttons_states() {
-	for(int i=0;i<N_BUTTON;i++) {
+	for(int i = 0; i < N_BUTTON; i++) {
 		pressed_state[i] = !((*buttons_mem) & (1 << i));
 	}
 }
