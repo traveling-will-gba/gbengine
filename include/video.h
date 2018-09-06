17c1da2a8ab3c23e802f41963c96106ef152eb59
Vitor Barbosa <vitornga15@gmail.com>
2018-09-05 23:15:33 -0300

    Add function to set sprite attributes

--- include/video.h
+++ include/video.h
@@ -7,14 +7,34 @@
 
 uint8_t *sprite_pal;
 
+struct attr {
+    uint8_t y;
+    uint8_t om : 2;
+    uint8_t gm : 2;  
+    uint8_t mos : 1;
+    uint8_t cm : 1;
+    uint8_t sh : 2;
+
+    uint16_t x : 9;
+    uint8_t aid : 5;
+    uint8_t sz : 2;
+
+    uint16_t tid : 10; 
+    uint8_t pr : 2;
+    uint8_t pb : 4;
+
+    uint16_t filler;
+};
+
 void reset_dispcnt();
 void set_video_mode(int video_mode);
 void enable_background(int background);
 void set_background(const void *pal, int pal_len, const void *tiles, int tiles_len, const void *map, int map_len);
 
 void init_sprite_mem();
+void set_sprite_attrs(int sprite_idx, struct attr *custom_attrs);
 bool set_sprite_pal(const void *pal, int pal_len);
-bool set_sprite(const void *tiles, int tiles_len, int *sprite_used);
+bool set_sprite(const void *tiles, int tiles_len, int *tile_used);
 void set_sprite_attr(uint32_t sprite_idx);
 
 // --- secondary typedefs ---
