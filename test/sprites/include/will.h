88979c6263b3df00669d572d211ff0cec2cf022d
Vitor Barbosa <vitornga15@gmail.com>
2018-09-11 20:48:11 -0300

    Add test image and remove O2 flag from Makefile

---
+++ test/sprites/include/will.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(will)
+
+//======================================================================
+//
+//	will, 64x64@8, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 512 + 4096 = 4608
+//
+//	Time-stamp: 2018-09-06, 22:58:01
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WILL_H
+#define GRIT_WILL_H
+
+#define willTilesLen 4096
+extern const unsigned int willTiles[1024];
+
+#define willPalLen 512
+extern const unsigned short willPal[256];
+
+#endif // GRIT_WILL_H
+
+//}}BLOCK(will)
