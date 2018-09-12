13920f7489e9323011786acbf4db72bdbf9fa9a8
Vitor Barbosa <vitornga15@gmail.com>
2018-09-11 21:53:16 -0300

    Add test sprite 16x16 and spritesheet for will

---
+++ test/sprites/include/will_idle.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(will_idle)
+
+//======================================================================
+//
+//	will_idle, 96x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 24 tiles not compressed
+//	Total size: 512 + 1536 = 2048
+//
+//	Time-stamp: 2018-09-11, 21:49:53
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WILL_IDLE_H
+#define GRIT_WILL_IDLE_H
+
+#define will_idleTilesLen 1536
+extern const unsigned int will_idleTiles[384];
+
+#define will_idlePalLen 512
+extern const unsigned short will_idlePal[256];
+
+#endif // GRIT_WILL_IDLE_H
+
+//}}BLOCK(will_idle)
