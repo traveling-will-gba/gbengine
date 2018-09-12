9c6186d281b3d2583dc97d1e2fca10f0332593f9
Vitor Barbosa <vitornga15@gmail.com>
2018-09-11 21:53:16 -0300

    Add test sprite 16x16 and spritesheet for will

---
+++ test/sprites/include/will16.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(will16)
+
+//======================================================================
+//
+//	will16, 16x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles not compressed
+//	Total size: 512 + 256 = 768
+//
+//	Time-stamp: 2018-09-11, 21:47:28
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WILL16_H
+#define GRIT_WILL16_H
+
+#define will16TilesLen 256
+extern const unsigned int will16Tiles[64];
+
+#define will16PalLen 512
+extern const unsigned short will16Pal[256];
+
+#endif // GRIT_WILL16_H
+
+//}}BLOCK(will16)
