53ca58e94c387b9c7249fbeaddade04ced1fceea
Vitor Barbosa <vitornga15@gmail.com>
2018-11-29 09:38:08 -0200

    Change menu image

--- test/game/include/menu.h
+++ test/game/include/menu.h
@@ -4,13 +4,13 @@
 //======================================================================
 //
 //	menu, 256x160@4, 
-//	Transparent palette entry: 17.
+//	Transparent palette entry: 50.
 //	+ palette 16 entries, not compressed
-//	+ 384 tiles (t|f reduced) not compressed
+//	+ 458 tiles (t|f reduced) not compressed
 //	+ regular map (flat), not compressed, 32x20 
-//	Total size: 32 + 12288 + 1280 = 13600
+//	Total size: 32 + 14656 + 1280 = 15968
 //
-//	Time-stamp: 2018-11-29, 08:56:12
+//	Time-stamp: 2018-11-29, 09:37:01
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -19,8 +19,8 @@
 #ifndef GRIT_MENU_H
 #define GRIT_MENU_H
 
-#define menuTilesLen 12288
-extern const unsigned int menuTiles[3072];
+#define menuTilesLen 14656
+extern const unsigned int menuTiles[3664];
 
 #define menuMapLen 1280
 extern const unsigned short menuMap[640];
--- test/game/src/main.cpp
+++ test/game/src/main.cpp
@@ -7,8 +7,6 @@
 #include "will_idle.h"
 #include "collectable.h"
 #include "metr.h"
-#include "platform_base.h"
-#include "platform_middle.h"
 
 #include "tw_will.h"
 #include "tw_game.h"
@@ -16,14 +14,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
-
-#include <vector>
-
-//FIXME Remove this include
-#include "physics.h"
-
-#define REG_BG2HOFS         *(vu16*)(REG_BASE+0x0018)
-#define REG_BG2VOFS         *(vu16*)(REG_BASE+0x001A)
 
 #define LEVEL_MENU 0
 #define LEVEL_1 1
