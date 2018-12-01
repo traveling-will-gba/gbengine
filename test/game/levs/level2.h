fb35dd93f5c304c9911a2b9074947ab3d81fc0d5
Vitor Barbosa <vitornga15@gmail.com>
2018-12-01 05:10:03 -0200

    Remove levs folder

--- test/game/levs/level2.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEVEL2_H
-#define LEVEL2_H
-
-#define level2_tempo 100
-
-#define level2_len 220
-
-#endif
--- test/game/levs/level5.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEVEL5_H
-#define LEVEL5_H
-
-#define level5_tempo 120
-
-#define level5_len 1580
-
-#endif
