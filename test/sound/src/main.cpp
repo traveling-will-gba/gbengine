0df217fbcd40c9ef6475f4232c3df3376935a7a3
Vitor Barbosa <vitornga15@gmail.com>
2018-12-04 09:01:48 -0200

    Fix sound and input demos

--- test/sound/src/main.cpp
+++ test/sound/src/main.cpp
@@ -1,6 +1,5 @@
 #include "input.h"
 #include "video.h"
-#include "sprite.h"
 #include "utils.h"
 #include "sound.h"
 
@@ -11,7 +10,7 @@ int main(){
     set_video_mode(0);
     enable_background(2);
 
-    Sound *s = new Sound();
+    Sound *s = Sound::get_sound();
 
     s->load_from_file(fase5_notes_len, fase5_notes, fase5_tempos_len, fase5_tempos);
     s->play();
