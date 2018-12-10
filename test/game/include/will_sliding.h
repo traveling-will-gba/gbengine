d211e29c5da8a65a0b37bb9518a23b171286a552
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2018-12-10 11:08:47 -0200

    Add sliding

---
+++ test/game/include/will_sliding.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(will_sliding)
+
+//======================================================================
+//
+//	will_sliding, 96x8@4, 
+//	+ palette 256 entries, not compressed
+//	+ 12 tiles Metatiled by 2x1 not compressed
+//	Total size: 512 + 384 = 896
+//
+//	Time-stamp: 2018-12-08, 21:18:41
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WILL_SLIDING_H
+#define GRIT_WILL_SLIDING_H
+
+#define will_slidingTilesLen 384
+extern const unsigned int will_slidingTiles[96];
+
+#define will_slidingPalLen 512
+extern const unsigned short will_slidingPal[256];
+
+#endif // GRIT_WILL_SLIDING_H
+
+//}}BLOCK(will_sliding)
--- test/game/src/tw_will.cpp
+++ test/game/src/tw_will.cpp
@@ -4,26 +4,37 @@
 
 #include "will_idle.h"
 #include "will_jumping.h"
+#include "will_sliding.h"
 
 #include "physics.h"
 
 #include "input.h"
 
 const int will_width = 16;
-const int will_height = 16;
+int will_height = 16;
 
 TWWill::TWWill(int x, int y) {
     m_x_speed = 0;
     m_y_speed = 0;
 
-    m_texture = new Texture(6, will_idlePal, will_idlePalLen, will_idleTiles, will_idleTilesLen, _4BPP);
+    m_texture[RUNNING] = new Texture(6, will_idlePal, will_idlePalLen, will_idleTiles, will_idleTilesLen, _4BPP);
+    m_texture[SLIDING] = new Texture(6, will_slidingPal, will_slidingPalLen, will_slidingTiles, will_slidingTilesLen, _4BPP);
 
-    m_texture->metadata.aid = 0;
-    m_texture->metadata.pr = 0;
-    m_texture->metadata.cm = 0;
-    m_texture->metadata.om = 0;
-    m_texture->metadata.sh = 0; // square
-    m_texture->metadata.sz = 1;
+    m_texture[RUNNING]->metadata.aid = 0;
+    m_texture[RUNNING]->metadata.pr = 0;
+    m_texture[RUNNING]->metadata.cm = 0;
+    m_texture[RUNNING]->metadata.om = 0;
+    m_texture[RUNNING]->metadata.sh = 0; // square
+    m_texture[RUNNING]->metadata.sz = 1;
+    m_texture[RUNNING]->metadata.om = 0;
+
+    m_texture[SLIDING]->metadata.aid = 0;
+    m_texture[SLIDING]->metadata.pr = 0;
+    m_texture[SLIDING]->metadata.cm = 0;
+    m_texture[SLIDING]->metadata.om = 0;
+    m_texture[SLIDING]->metadata.sh = 1;
+    m_texture[SLIDING]->metadata.sz = 0;
+    m_texture[SLIDING]->metadata.om = 2;
 
     set_x(x);
     set_y(y);
@@ -41,7 +52,8 @@ void TWWill::draw_self() {
 }
 
 void TWWill::update_self(uint64_t dt) {
-    m_texture->update(dt);
+    m_texture[RUNNING]->update(dt);
+    m_texture[SLIDING]->update(dt);
 
     set_x(m_x + m_x_speed);
     set_y(m_y + m_y_speed);
@@ -53,6 +65,7 @@ void TWWill::update_self(uint64_t dt) {
             m_jump_counter = 0;
             check_falling();
             check_jumping();
+            check_sliding();
             break;
         case JUMPING:
             m_y_speed++;
@@ -61,6 +74,10 @@ void TWWill::update_self(uint64_t dt) {
         case FALLING:
             check_running();
             check_jumping();
+            break;
+        case SLIDING:
+            check_falling();
+            check_sliding();
             break;
         default:
             break;
@@ -71,46 +88,63 @@ void TWWill::update_self(uint64_t dt) {
 }
 
 void TWWill::check_running() {
-    // print("check_running\n");
-    for (size_t i = 0; i < colliding_plats.size(); i++) {
-        //if (m_colliding) {
-            // print("colidindo com plataforma de y %d\n", colliding_plats[i]->y());
-            // print("will na altura %d\n", m_y);
-        //}
+    if (m_state == FALLING) {
+        for (size_t i = 0; i < colliding_plats.size(); i++) {
+            if (m_y + will_height >= colliding_plats[i]->y() && m_y + will_height <= colliding_plats[i]->y() + 3) {
+                m_y_speed = 0;
 
-        if (/*m_colliding &&*/ (m_y + will_height >= colliding_plats[i]->y() && m_y + will_height <= colliding_plats[i]->y() + 3)) {
-            // print("set_running %d\n", m_y_speed);
+                will_height = 16;
+                set_y(colliding_plats[i]->y() - will_height);
 
-            m_y_speed = 0;
+                m_texture[RUNNING]->metadata.om = 0;
+                m_texture[SLIDING]->metadata.om = 2;
 
-            set_y(colliding_plats[i]->y() - will_height);
-
-            m_state = RUNNING;
-            break;
+                m_state = RUNNING;
+                break;
+            }
         }
     }
 }
 
 void TWWill::check_jumping() {
-    // print("check_jumping\n");
     if ((pressed(BUTTON_UP) || pressed(BUTTON_A)) && m_jump_counter < 3) {
-        // print("set_jumping\n");
         m_y_speed = -7;
         m_jump_counter++;
         m_state = JUMPING;
     }
 }
 
+void TWWill::check_sliding() {
+    if(not pressing(BUTTON_DOWN)) return;
+
+    if (m_state == FALLING) {
+        for (size_t i = 0; i < colliding_plats.size(); i++) {
+            if (m_y + will_height >= colliding_plats[i]->y() && m_y + will_height <= colliding_plats[i]->y() + 3) {
+                m_y_speed = 0;
+
+                will_height = 8;
+                set_y(colliding_plats[i]->y() - will_height);
+
+                m_texture[RUNNING]->metadata.om = 2;
+                m_texture[SLIDING]->metadata.om = 0;
+
+                m_state = SLIDING;
+                break;
+            }
+        }
+    }
+}
+
 void TWWill::check_falling() {
-    // print("check_falling\n");
     if(m_state == JUMPING && m_y_speed >= 0) {
-        // print("set_falling\n");
         m_state = FALLING;
         m_y_speed = 1;
-    } else if (m_state == RUNNING) {
-        // print("set falling\n");
+    }else if (m_state == RUNNING) {
         m_state = FALLING;
-        m_y_speed = 2;
+        m_y_speed = 1;
+    } else if (m_state == SLIDING) {
+        m_state = FALLING;
+        m_y_speed = 1;
     }
 }
 
@@ -118,11 +152,8 @@ void TWWill::on_collision(const Collidable *who) {
     m_colliding = false;
 
     if (auto col = dynamic_cast <const TWCollectable *>(who)) {
-        // print("Colidiu com coletavel\n");
         m_items_collected++;
-        // print("COLETAVEIS PEGOS %d\n", m_items_collected);
     } else if (auto platform = dynamic_cast <const TWPlatform *>(who)) {
-        // print("Colidiu com plataforma\n");
         m_colliding = true;
 
         colliding_plats.push_back(platform);
@@ -132,12 +163,14 @@ void TWWill::on_collision(const Collidable *who) {
 
 void TWWill::set_x(int x) {
     m_x = x;
-    m_texture->metadata.x = m_x;
+    m_texture[RUNNING]->metadata.x = m_x;
+    m_texture[SLIDING]->metadata.x = m_x;
 
 }
 void TWWill::set_y(int y) {
     m_y = y;
-    m_texture->metadata.y = m_y;
+    m_texture[RUNNING]->metadata.y = m_y;
+    m_texture[SLIDING]->metadata.y = m_y;
 }
 
 void TWWill::set_speed_x(int x) {
